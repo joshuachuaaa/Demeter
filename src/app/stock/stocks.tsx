c504ae31ef0a0f37cc6c9eb52c97c54db57e2cc5
Joshua Chua <joshuachua2000@hotmail.com>
2025-01-20 23:23:14 +1100

    Updated stocks.tsx of tracked stock list

---
+++ src/app/stock/stocks.tsx
@@ -0,0 +1,55 @@
+// src/stocks/stocksList.ts
+import { StockSubject } from './stocktypes';
+
+export const STOCKS_LIST: StockSubject[] = [
+  {
+    logo: "/aapl.svg", // Relative path to the logo in the public folder
+    name: "Apple Inc.",
+    symbol: "AAPL",
+  },
+  {
+    logo: "/msft.svg",
+    name: "Microsoft Corporation",
+    symbol: "MSFT",
+  },
+  {
+    logo: "/googl.svg",
+    name: "Alphabet Inc.",
+    symbol: "GOOGL",
+  },
+  {
+    logo: "/amzn.svg",
+    name: "Amazon.com, Inc.",
+    symbol: "AMZN",
+  },
+  {
+    logo: "/tsla.svg",
+    name: "Tesla, Inc.",
+    symbol: "TSLA",
+  },
+  {
+    logo: "/nflx.svg",
+    name: "Netflix, Inc.",
+    symbol: "NFLX",
+  },
+  {
+    logo: "/nvda.svg",
+    name: "NVIDIA Corporation",
+    symbol: "NVDA",
+  },
+  {
+    logo: "/meta.svg",
+    name: "Meta Platforms, Inc.",
+    symbol: "META",
+  },
+  {
+    logo: "/brkb.svg",
+    name: "Berkshire Hathaway Inc.",
+    symbol: "BRK.B",
+  },
+  {
+    logo: "/jnj.svg",
+    name: "Johnson & Johnson",
+    symbol: "JNJ",
+  },
+];
